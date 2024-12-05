34fcc99c334c888f03bec7d76cdac2b0524053be
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2024-12-05 13:04:42 +0000

    step 3: refactor to unittest.TestCase

--- test_register_csinc_ie.py
+++ test_register_csinc_ie.py
@@ -1,16 +1,29 @@
 import time
+import unittest
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-driver.get("http://register.csinc.ie/")
 
-assert "Log in to CSLinc" in driver.title
+class TestLoginPage(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.get("http://register.csinc.ie/")
+        # Wait for the contents to load...
+        self.browser.implicitly_wait(10)
 
-# Email element
-email_element = driver.find_element(By.ID, "Email")
-email_element.send_keys("nobody@example.com")
-assert "nobody@example.com" == email_element.get_attribute("value")
+    def tearDown(self):
+        self.browser.close()
 
-driver.close()
+    def test_login_page(self):
+        # We should be on the login page:
+        self.assertIn("Log in to CSLinc", self.browser.title)
+
+        # Email element
+        email_element = self.browser.find_element(By.ID, "Email")
+        email_element.send_keys("nobody@example.com")
+        self.assertEqual("nobody@example.com", email_element.get_attribute("value"))
+
+
+if __name__ == "__main__":
+    unittest.main()
